7ad850faaef391f7e82b7e565d6d20cc9684b099
Aditya Rathore <98386784+Adi-The-Pro@users.noreply.github.com>
2024-08-09 14:52:51 +0530

    Delete frontend directory

--- frontend/src/components/shared/NavigationLink.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-export default function NavigationLink({to,text,onClick}) {
-  return (
-    <Link onClick={onClick} to={to}>
-      {text}
-    </Link>
-  )
-}
--- frontend/src/components/shared/TextInput.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './TextInput.module.css'
-export default function TextInput({type,placeholder,onChange}) {
-  return (
-    <input className={styles.input} onChange={onChange} type={type} placeholder={placeholder}>
-    </input>  
-  )
-}
